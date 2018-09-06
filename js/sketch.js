b9ea614d42b53e2192f3401bdefdd4587336396d
Joshua Burke <burke.joshua.james@gmail.com>
2018-09-06 17:21:32 -0400

    Make the sketch javascript draw faster and reload after a delay

--- js/sketch.js
+++ js/sketch.js
@@ -1,8 +1,30 @@
+'use-strict';
+
 var img;
 var smallPoint, largePoint;
+const images = [
+  'team1',
+  'team2',
+  'team3',
+  'team4',
+  'team5',
+  'founders',
+];
+
+const pickImage = (imgArray) => {
+  const index = getRandomIntInclusive(0, 5);
+  return imgArray[index];
+};
+
+const resetPageAfterDelay = (delay) => {
+  window.setTimeout(() => {
+    window.location.reload(false)
+  }, parseInt(delay, 10));
+}
 
 function preload() {
-  img = loadImage('../images/moon-walk-flag-small.jpg');
+  image = pickImage(images);
+  img = loadImage(`../images/${image}.jpg`);
 }
 
 function setup() {
@@ -13,13 +35,23 @@ function setup() {
   noStroke();
   background(255);
   img.loadPixels();
+  resetPageAfterDelay(25000);
 }
 
 function draw() {
-  var pointillize = map(500, 0, width, smallPoint, largePoint);
-  var x = floor(random(img.width));
-  var y = floor(random(img.height));
-  var pix = img.get(x, y);
-  fill(pix, 128);
-  ellipse(x, y, pointillize, pointillize);
+  for (let i = 0; i < 40; i++) {
+    let pointillize = map(200, 0, width, smallPoint, largePoint);
+    let x = floor(random(img.width));
+    let y = floor(random(img.height));
+    let pix = img.get(x, y);
+    fill(pix, 128);
+    ellipse(x, y, pointillize, pointillize);
+  }
+}
+
+function getRandomIntInclusive(min, max) {
+  //The maximum is inclusive and the minimum is inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
